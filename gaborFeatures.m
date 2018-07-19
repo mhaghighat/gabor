93e29394a4fd992a82451bfb91a8899f8c207119
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2018-07-19 12:45:09 +0100

    minor

--- gaborFeatures.m
+++ gaborFeatures.m
@@ -86,20 +86,20 @@ end
 
 %% Show filtered images (Please comment this section if not needed!)
 
-% % Show real parts of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Real parts of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(real(gaborResult{i,j}),[]);
-%     end
-% end
-% 
-% % Show magnitudes of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(abs(gaborResult{i,j}),[]);
-%     end
-% end
+% Show real parts of Gabor-filtered images
+figure('NumberTitle','Off','Name','Real parts of Gabor filters');
+for i = 1:u
+    for j = 1:v        
+        subplot(u,v,(i-1)*v+j)    
+        imshow(real(gaborResult{i,j}),[]);
+    end
+end
+
+% Show magnitudes of Gabor-filtered images
+figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
+for i = 1:u
+    for j = 1:v        
+        subplot(u,v,(i-1)*v+j)    
+        imshow(abs(gaborResult{i,j}),[]);
+    end
+end
